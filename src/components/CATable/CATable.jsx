5a5fdac1d86eb858c0a73af8954da916fa1b4400
Muskan Singh <muskansingh@Muskans-MacBook-Air.local>
2021-05-20 16:41:05 +0100

    updated CA page

--- src/components/CATable/CATable.jsx
+++ src/components/CATable/CATable.jsx
@@ -3,7 +3,46 @@ import styles from './CATable.module.scss';
 
 const CATable = () => {
     return (
-        <div></div>
+        <div className={styles.CATable}>
+            <section className={styles.tableHeadings1}>
+                <button className={styles.headingOption1}>Scores by question</button>
+                <button className={styles.headingOption2}>Year on year comparison</button>
+                <button className={styles.headingOption3}>Examples and comments</button>
+            </section>
+            <div className={styles.tableContent}>
+                
+                    <div className={styles.responseCount}>20 responses</div>
+                    <div className={styles.option1} >Struggles to meet expectations</div>
+                    <div className={styles.option2}>Could improve</div>
+                    <div className={styles.option3}>Meets expectations</div>
+                    <div className={styles.option4}>Exceeds Expectations</div>
+                    
+                    <p className={styles.question1}>Communicates well with customers?</p>
+                    <p className={styles.question2}>Can work autonomously?</p>
+                    <p className={styles.question3}>Acts professionally?</p>
+                    <p className={styles.question4}>Communicates well with colleagues?</p>
+                    
+              
+                
+                    <span className={styles.percentage1}>0%</span>
+                    <span className={styles.percentage2}>0%</span>
+                    <span className={styles.percentage3}>0%</span>
+                    <span className={styles.percentage4}>0%</span>
+                    <span className={styles.percentage5}>0%</span>
+                    <span className={styles.percentage6}>0%</span>
+                    <span className={styles.percentage7}>0%</span>
+                    <span className={styles.percentage8}>0%</span>
+                    <span className={styles.percentage9}>0%</span>
+                    <span className={styles.percentage10}>0%</span>
+                    <span className={styles.percentage11}>0%</span>
+                    <span className={styles.percentage12}>0%</span>
+                    <span className={styles.percentage13}>0%</span>
+                    <span className={styles.percentage14}>0%</span>
+                    <span className={styles.percentage15}>0%</span>
+                    <span className={styles.percentage16}>0%</span>
+                
+            </div>
+        </div>
     );
 };
 export default CATable;
--- src/pages/closedappraisals.jsx
+++ src/pages/closedappraisals.jsx
@@ -3,14 +3,17 @@ import Navbar from '../components/Navbar';
 import ClosedAppraisalsHeader from '../components/ClosedAppraisalsHeader';
 import CASideMenu from '../components/CASideMenu';
 import CATable from '../components/CATable';
+import styles from './closedappraisals.module.scss'
 
 const ClosedAppraisals = () => {
     return (
         <>
         <Navbar />
         <ClosedAppraisalsHeader />
+        <div className={styles.CAContainer}>
         <CASideMenu />
         <CATable />
+        </div>
         </>
     );
 };
