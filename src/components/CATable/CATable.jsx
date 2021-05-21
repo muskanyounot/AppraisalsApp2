6dd9410b273ef1e932d14f4ccf0bd3d87a8d0d54
Muskan Singh <muskansingh@Muskans-MacBook-Air.local>
2021-05-21 02:45:49 +0100

    updated CA page

--- src/components/CATable/CATable.jsx
+++ src/components/CATable/CATable.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './CATable.module.scss';
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Button,
+    Image
+} from 'react-native';
 
 const CATable = () => {
+
+    const [shouldShow1, setShouldShow1] = useState(true);
+    const [shouldShow2, setShouldShow2] = useState(false);
+    const [shouldShow3, setShouldShow3] = useState(true);
+
     return (
-        <div className={styles.CATable}>
-            <section className={styles.tableHeadings1}>
-                <button className={styles.headingOption1}>Scores by question</button>
-                <button className={styles.headingOption2}>Year on year comparison</button>
-                <button className={styles.headingOption3}>Examples and comments</button>
+        <SafeAreaView style={{flex: 1}}>
+                    <View className={styles.CATable}>
+                    
+            <section className={styles.tableHeadings1}>   
+                    <Button
+                title="Scores by question"
+                className={styles.headingOption1}
+                onPress={() => setShouldShow1(!shouldShow1)}
+                />
+                <Button
+                title="Year on year comparison"
+                className={styles.headingOption2}
+                onPress={() => setShouldShow2(!shouldShow2)}
+                />
+                <Button
+                title="Examples and comments"
+                className={styles.headingOption3}
+                onPress={() => setShouldShow3(!shouldShow3)}
+                />
             </section>
+
+            {shouldShow1 ? (
+        
             <div className={styles.tableContent}>
                 
                     <div className={styles.responseCount}>20 responses</div>
@@ -41,8 +70,75 @@ const CATable = () => {
                     <span className={styles.percentage15}>0%</span>
                     <span className={styles.percentage16}>0%</span>
                 
-            </div>
-        </div>
+            </div> 
+            ) : null}
+
+            {shouldShow2 ? (
+                <div>
+                    <label className={styles.compare}>Comparing to:</label>
+                    <select className={styles.yearCompare}>
+                       <option>2021</option> 
+                       <option>2022</option>
+                       <option>2023</option>
+                    </select>
+
+                    <div className={styles.barChart}>
+
+                        <p className={styles.BCHeading1}>Exceeds Expectations</p>
+                        <p className={styles.BCHeading2}>Meets Expectations</p>
+                        <p className={styles.BCHeading3}>Could Improve</p>
+                        <p className={styles.BCHeading4}>Underperforming</p>
+
+                        <span className={styles.BCYear1}>
+                            <div className={styles.bar1}>2023</div>
+                            <div className={styles.freespace1}></div>
+                        </span>
+
+                        <span className={styles.BCYear2}>
+                            <div className={styles.bar2}>2022</div>
+                            <div className={styles.freespace2}></div>
+                        </span>
+
+                        <span className={styles.BCYear3}>
+                            <div className={styles.bar3}>2023</div>
+                            <div className={styles.freespace3}></div>
+                        </span>
+                        
+                        <span className={styles.BCYear4}>
+                            <div className={styles.bar4}>2022</div>
+                            <div className={styles.freespace4}></div>
+                        </span>
+
+                        <span className={styles.BCYear5}>
+                            <div className={styles.bar5}>2023</div>
+                            <div className={styles.freespace5}></div>
+                        </span>
+
+                        <span className={styles.BCYear6}>
+                            <div className={styles.bar6}>2022</div>
+                            <div className={styles.freespace6}></div>
+                        </span>
+
+                        <span className={styles.underperform1}>2</span>
+                        <span className={styles.underperform2}>in 2022</span>
+
+                        <span className={styles.underperform3}>1</span>
+                        <span className={styles.underperform4}>in 2023</span>
+
+                        <span className={styles.BCpercent1}>60%</span>
+                        <span className={styles.BCpercent2}>50%</span>
+
+                        <span className={styles.BCpercent3}>90%</span>
+                        <span className={styles.BCpercent4}>80%</span>
+
+                        <span className={styles.BCpercent5}>2%</span>
+                        <span className={styles.BCpercent6}>5%</span>
+                    </div>
+                </div>
+         ) : null}
+        </View>
+        </SafeAreaView>
+
     );
 };
 export default CATable;
